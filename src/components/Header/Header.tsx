59dc3f5eb9530b761e204e0bd1644036453ef5c6
Max <48150531+mashtakovmm@users.noreply.github.com>
2023-11-02 11:55:48 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import "./Header.css"
+
+const Header:FC = () => {
+
+    return(
+        <header className='header'>
+            <h2 className='logo'>Korabli</h2>
+        </header>
+    )
+}
+
+export default Header
